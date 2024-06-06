cd4d4ade7530b23e69488e81bc884e0ba3f3c3c5
MoHaulik <88561887+MoHaulik@users.noreply.github.com>
2024-06-06 23:13:10 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const icons = document.querySelectorAll('.icon');
     const clickSound = document.getElementById('click-sound');
+    const hoverSound = document.getElementById('hover-sound');
 
     icons.forEach(icon => {
         icon.addEventListener('click', () => {
             clickSound.play();
+        });
+
+        icon.addEventListener('mouseenter', () => {
+            hoverSound.currentTime = 0;
+            hoverSound.play();
         });
     });
 });
